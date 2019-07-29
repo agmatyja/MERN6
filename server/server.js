079ed0c436af3f2ef84b4cc9e626b962ac4445fd
Agnieszka Matyja <agmatyja@gmail.com>
2019-07-29 06:02:37 +0200

    static serving works locally

--- server/server.js
+++ server/server.js
@@ -12,16 +12,16 @@ const app = express();
 // import routes
 const postRoutes = require('./routes/post.routes');
 app.use(cors());
+app.use(helmet());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use('/api', postRoutes);
-app.use(helmet());
-app.use(sanitize);
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/../client/build/index.html'));
-}); 
+// app.use(sanitize);
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, '/../client/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/../client/build/index.html'));
+});
 
 // connects our back end code with the database
 mongoose.connect(config.DB, { useNewUrlParser: true });
